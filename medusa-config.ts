76e3559b6eb04ce7da8f09b6f01b61edb87363f2
Kilvio Lluberes <60864656+kilviolluberes@users.noreply.github.com>
2025-06-30 14:36:04 -0500

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,1 +1,19 @@
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL, // Provided by Medusa Cloud
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET // Set in Medusa Cloud
+      cookieSecret: process.env.COOKIE_SECRET // Set in Medusa Cloud
+    },
+  },
+  admin: {
+      backendUrl: process.env.MEDUSA_BACKEND_URL
+  },
+})
